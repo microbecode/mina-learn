b9882f87e8fd259485489abef4a787f97b466a0a
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2024-01-16 12:18:40 +0000

    add deposit

--- src/MyTree.test.ts
+++ src/MyTree.test.ts
@@ -1,107 +1,18 @@
 import {
   AccountUpdate,
   Field,
+  Gadgets,
   MerkleTree,
   MerkleWitness,
   Mina,
   Poseidon,
   PrivateKey,
+  Provable,
   PublicKey,
 } from 'o1js';
 import { MyTree } from './MyTree';
 
 let proofsEnabled = false;
-
-xdescribe('MyTree', () => {
-  let deployerAccount: PublicKey,
-    deployerKey: PrivateKey,
-    senderAccount: PublicKey,
-    senderKey: PrivateKey,
-    zkAppAddress: PublicKey,
-    zkAppPrivateKey: PrivateKey,
-    zkApp: MyTree;
-
-  let tree: MerkleTree;
-
-  const height = 7;
-
-  class MerkleWitness7 extends MerkleWitness(height) {}
-
-  beforeAll(async () => {
-    const Local = Mina.LocalBlockchain({ proofsEnabled });
-    Mina.setActiveInstance(Local);
-    ({ privateKey: deployerKey, publicKey: deployerAccount } =
-      Local.testAccounts[0]);
-    ({ privateKey: senderKey, publicKey: senderAccount } =
-      Local.testAccounts[1]);
-
-    zkAppPrivateKey = PrivateKey.random();
-    zkAppAddress = zkAppPrivateKey.toPublicKey();
-    zkApp = new MyTree(zkAppAddress);
-
-    if (proofsEnabled) await MyTree.compile();
-
-    tree = new MerkleTree(height);
-
-    // deploy the smart contract
-    const deployTxn = await Mina.transaction(deployerAccount, () => {
-      AccountUpdate.fundNewAccount(deployerAccount);
-      zkApp.deploy();
-      // get the root of the new tree to use as the initial tree root
-      zkApp.initState(tree.getRoot());
-    });
-    await deployTxn.prove();
-    deployTxn.sign([deployerKey, zkAppPrivateKey]);
-
-    await deployTxn.send();
-  });
-
-  it('can store a hundred values locally', async () => {
-    // create a new tree
-
-    const incrementIndex = 2n;
-    const incrementAmount = Field(9);
-
-    // get the witness for the current tree
-    let witness = new MerkleWitness7(tree.getWitness(incrementIndex));
-
-    // update the leaf locally
-    tree.setLeaf(incrementIndex, incrementAmount);
-
-    /*  // update the smart contract
-    const txn1 = await Mina.transaction(senderAccount, () => {
-      zkApp.update(
-        witness,
-        Field(0), // leafs in new trees start at a state of 0
-        incrementAmount
-      );
-    });
-    await txn1.prove();
-    const pendingTx = await txn1.sign([senderKey, zkAppPrivateKey]).send();
-    await pendingTx.wait(); */
-    const aaa = tree.getNode(6, 2n);
-
-    console.log(
-      'node value, hashed',
-      aaa.value,
-      Poseidon.hash([incrementAmount])
-    );
-
-    witness = new MerkleWitness7(tree.getWitness(incrementIndex));
-    const computedRoot = witness.calculateRoot(
-      Poseidon.hash([incrementAmount])
-    );
-
-    console.log('ARE EQUAL?', computedRoot, tree.getRoot());
-    // compare the root of the smart contract tree to our local tree
-    console.log(
-      `BasicMerkleTree: local tree root hash after send1: ${tree.getRoot()}`
-    );
-    console.log(
-      `BasicMerkleTree: smart contract root hash after send1: ${zkApp.treeRoot.get()}`
-    );
-  });
-});
 
 describe('MyTree', () => {
   let deployerAccount: PublicKey,
@@ -118,7 +29,7 @@ describe('MyTree', () => {
 
   class MerkleWitness8 extends MerkleWitness(height) {}
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const Local = Mina.LocalBlockchain({ proofsEnabled });
     Mina.setActiveInstance(Local);
     ({ privateKey: deployerKey, publicKey: deployerAccount } =
@@ -166,15 +77,15 @@ describe('MyTree', () => {
     }
   });
 
-  it('can store a hundred values in the contract', async () => {
-    for (let i = 0; i < 100; i++) {
+  xit('can store a hundred values in the contract', async () => {
+    for (let i = 0; i < 3; i++) {
       const txn = await Mina.transaction(senderAccount, () => {
         const newValue = Field(i + 1);
         tree.setLeaf(BigInt(i), newValue);
 
         let witness = new MerkleWitness8(tree.getWitness(BigInt(i)));
 
-        zkApp.update(
+        zkApp.addValue(
           witness,
           Field(0), // leafs in new trees start at a state of 0
           newValue
@@ -182,6 +93,92 @@ describe('MyTree', () => {
       });
       await txn.prove();
       await txn.sign([senderKey, zkAppPrivateKey]).send();
+
+      zkApp.treeRoot.get().assertEquals(tree.getRoot(), 'not matching roots');
     }
+  });
+
+  it('can deposit a secret', async () => {
+    const secret = Field(33);
+
+    const txn = await Mina.transaction(senderAccount, () => {
+      zkApp.deposit(secret);
+    });
+    await txn.prove();
+    await txn.sign([senderKey, zkAppPrivateKey]).send();
+  });
+});
+
+xdescribe('other', () => {
+  it('hmm', async () => {
+    const secret = Field(18);
+
+    let flag1True = Provable.if(
+      Gadgets.and(secret, Field(32), 6).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag2True = Provable.if(
+      Gadgets.and(secret, Field(16), 5).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag3True = Provable.if(
+      Gadgets.and(secret, Field(8), 4).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag4True = Provable.if(
+      Gadgets.and(secret, Field(4), 3).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag5True = Provable.if(
+      Gadgets.and(secret, Field(2), 20).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag6True = Provable.if(
+      Gadgets.and(secret, Field(1), 1).equals(0),
+      Field(0),
+      Field(1)
+    );
+
+    console.log(
+      'flags',
+      '\n',
+      flag1True,
+      '\n',
+      flag2True,
+      '\n',
+      flag3True,
+      '\n',
+      flag4True,
+      '\n',
+      flag5True,
+      '\n',
+      flag6True
+    );
+
+    // If flag 1 is true, then all other flags must be false
+    // flag1True * ((1 - flag2True) + (1 - flag2True) + ... (1 - flag6True)) = 0
+    flag1True
+      .mul(
+        Field(1)
+          .sub(flag2True)
+          .add(Field(1).sub(flag3True))
+          .add(Field(1).sub(flag4True))
+          .add(Field(1).sub(flag5True))
+          .add(Field(1).sub(flag6True))
+      )
+      .equals(Field(0));
+
+    // If flag 2 is true, then flag 3 must also be true.
+    // flag2True * (1 - flag3True) = 0
+    flag2True.mul(Field(1).sub(flag3True)).equals(Field(0));
+
+    // If flag 4 is true, then flags 5 and 6 must be false.
+    // flag4True * (flag5True + flag6True) = 0
+    flag4True.mul(flag5True.add(flag6True)).equals(Field(0));
   });
 });
--- src/MyTree.ts
+++ src/MyTree.ts
@@ -6,18 +6,27 @@ import {
   method,
   MerkleWitness,
   PublicKey,
+  Gadgets,
+  circuitMain,
+  Circuit,
+  Provable,
 } from 'o1js';
 
 class MerkleWitness8 extends MerkleWitness(8) {}
 
 export class MyTree extends SmartContract {
   @state(Field) treeRoot = State<Field>();
+  @state(Field) messagesReceived = State<Field>();
+
+  events = {
+    'message-received': Field,
+  };
 
   @method initState(initialRoot: Field) {
     this.treeRoot.set(initialRoot);
   }
 
-  @method update(
+  @method addValue(
     leafWitness: MerkleWitness8,
     valueBefore: Field,
     valueAfter: Field
@@ -34,5 +43,65 @@ export class MyTree extends SmartContract {
 
     // set the new root
     this.treeRoot.set(rootAfter);
+  }
+
+  @method deposit(secret: Field) {
+    let flag1True = Provable.if(
+      Gadgets.and(secret, Field(32), 6).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag2True = Provable.if(
+      Gadgets.and(secret, Field(16), 5).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag3True = Provable.if(
+      Gadgets.and(secret, Field(8), 4).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag4True = Provable.if(
+      Gadgets.and(secret, Field(4), 3).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag5True = Provable.if(
+      Gadgets.and(secret, Field(2), 20).equals(0),
+      Field(0),
+      Field(1)
+    );
+    let flag6True = Provable.if(
+      Gadgets.and(secret, Field(1), 1).equals(0),
+      Field(0),
+      Field(1)
+    );
+
+    // If flag 1 is true, then all other flags must be false
+    // flag1True * ((1 - flag2True) + (1 - flag2True) + ... (1 - flag6True)) = 0
+    flag1True
+      .mul(
+        Field(1)
+          .sub(flag2True)
+          .add(Field(1).sub(flag3True))
+          .add(Field(1).sub(flag4True))
+          .add(Field(1).sub(flag5True))
+          .add(Field(1).sub(flag6True))
+      )
+      .equals(Field(0));
+
+    // If flag 2 is true, then flag 3 must also be true.
+    // flag2True * (1 - flag3True) = 0
+    flag2True.mul(Field(1).sub(flag3True)).equals(Field(0));
+
+    // If flag 4 is true, then flags 5 and 6 must be false.
+    // flag4True * (flag5True + flag6True) = 0
+    flag4True.mul(flag5True.add(flag6True)).equals(Field(0));
+
+    const received = this.messagesReceived.get();
+    this.messagesReceived.requireEquals(received);
+    this.messagesReceived.set(received.add(1));
+
+    this.emitEvent('message-received', secret);
   }
 }
